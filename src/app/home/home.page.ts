d473a8a0fa366712a60652d15905115c3af6816b
Frank Bignone <frank@darkhand-studio.com>
2019-11-19 10:36:57 +0900

    initial release

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
+
 export class HomePage {
 
-  constructor() {}
+  tstObj: { key: string } = { key: 'a'};
 
+  gameLst: Array<{key: string; value: string}> = [];
+
+  constructor(private http: HttpClient) {}
+
+  ionViewDidEnter() {
+    this.http.get('./assets/data.json').subscribe( (settings: any) => {
+      this.gameLst = settings.gameLst;
+    });
+
+  }
+  onChangeVal() {
+
+  }
 }
